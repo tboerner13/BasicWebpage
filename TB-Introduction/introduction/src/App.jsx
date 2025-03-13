a409f581063d82d88ad0ddb16bd284ad36dbcb31
tboerner1 <tjb0013@gmail.com>
2025-03-13 12:32:35 -0500

    Header and sticky navigation

--- TB-Introduction/introduction/src/App.jsx
+++ TB-Introduction/introduction/src/App.jsx
@@ -8,15 +8,56 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-screen'>
+        <header className='bg-blue-500 w-full'>
+          <h1>Title</h1>
+        </header>
       </div>
+
+      <div className='w-screen sticky top-0'>
+        <ul className='flex space-x-4 bg-blue-800'>
+          <li className='text-white px-4 py-2 rounded w-1/2'>Home</li>
+          <li className='text-white px-4 py-2 rounded'>6th Grade</li>
+          <li className='text-white px-4 py-2 rounded'>7th Grade</li>
+          <li className='text-white px-4 py-2 rounded'>8th Grade</li>
+        </ul>
+      </div>
+
+
       <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
